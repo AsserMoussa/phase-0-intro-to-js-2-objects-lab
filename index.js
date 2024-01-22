ba195f8463a28330b703aa23e711c3f213ac9ea3
assermoussa <asser.y.moussa@gmail.com>
2024-01-21 23:48:20 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "OS",
+    streetAddress: "123 main",};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newemployee = {...employee};
+    newemployee[key] = value;
+    return newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newemp = {...employee};
+    delete newemp[key];
+    return newemp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
